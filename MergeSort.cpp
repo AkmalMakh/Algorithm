883adb5fe234de3bd5660f7deb5ea1c8ae83e8b8
Akmal Makhmudov <akmalmakhmudov@Akmals-MacBook-Pro.local>
2020-10-21 14:30:11 +0900

    Merge Sort with array

---
+++ MergeSort.cpp
@@ -0,0 +1,92 @@
+
+#include <iostream>
+
+using namespace std;
+
+
+
+void Merge(int A[], int start,int middle,int end){
+    
+    int n1 = middle-start+1; // plus 1 because our array starts from 0 
+    int n2 = end-middle; 
+   
+    int L[n1], R[n2];  // dividing into 2 parts our array
+    
+    for(int i = 0; i < n1; i++) // copping 
+        L[i] = A[start + i]; 
+    for(int j = 0; j < n2; j++) 
+        R[j] = A[middle + 1 + j]; 
+        
+    int i = 0, j= 0;
+         
+    int k = start; 
+    
+    while(i< n1 && j< n2){  //n1 is middle-start+1 while n2 is end-middle
+        if(L[i]<=R[j]){
+            A[k++]= L[i++]; 
+        }else{
+            A[k++] = R[j++];
+        }
+        
+        
+    }
+    while(i<n1){
+            A[k++] = L[i++];
+        }
+        while(j<n2){
+            A[k++] = R[j++];
+        }
+    
+}
+
+void printArray(int A[], int size) 
+{ 
+    for(int i = 0; i < size; i++) 
+        cout << A[i] << " "; 
+        
+    cout<<endl;    
+} 
+
+void MergeSort(int arr[],int start,int end){
+    if(start<end){
+        int middle = start+(end-start)/2; // dividing into 2 part our array
+        
+        MergeSort(arr,start,middle); //sorting left side  
+        MergeSort(arr,middle+1,end); //sorting right side 
+        
+        Merge(arr,start,middle,end);
+    }
+    
+}
+int main()
+{
+   int arr []={4,2,3,7,5};   
+    int arr_size = sizeof(arr) / sizeof(arr[0]); 
+   int n;
+   
+    
+   
+   do{
+        cout<<"Choose sorting algorithm 1: Merge Sort 4: Print Array "<<endl;
+        cin>>n;
+       switch(n){
+           
+           case 1: // merge sorting nlogn 
+                MergeSort(arr,0,arr_size-1);
+                break;
+           case 2: //Heap sorting  nlogn
+                break;
+           case 3: //quick sorting nlogn
+                break;
+           case 4: 
+                printArray(arr,arr_size);
+                break;
+           default:
+             break;
+       }
+   }while(n!=0);
+   
+   
+    
+    return 0;
+}
